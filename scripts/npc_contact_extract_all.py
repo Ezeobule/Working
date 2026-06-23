1d4ba904ca188e8ccebccf9c280bf1450bb23232
Omeje <137063621+Ezeobule@users.noreply.github.com>
2026-06-23 11:04:45 +0200

    Expand extraction nationwide

--- scripts/npc_contact_extract_all.py
+++ scripts/npc_contact_extract_all.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-import npc_contact_extract as extractor
+import npc_nationwide_extract as nationwide
 
-extractor.TARGET = 2000
-extractor.main()
+nationwide.extractor.OUTDIR = "npc_output"
+nationwide.extractor.TARGET = 10000
+nationwide.extractor.main()
